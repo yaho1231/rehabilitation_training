4f07babcc2a95cfa3c4945508df769dd6e8a3899
yaho1231 <hiimjungle@naver.com>
2024-02-14 19:01:40 +0900

    basic_setting

---
+++ rehabilitationtraining.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <stack>
+#include <queue>
+#include <utility>
+#include <cmath>
+#include <set>
+#define endl '\n'
+#include <stdio.h>
+#define INF 999999999
+using namespace std;
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
