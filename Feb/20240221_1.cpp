4da2e09c376d9dd450ab6b5a0b686ef9cdb36084
yaho1231 <hiimjungle@naver.com>
2024-02-21 21:11:28 +0900

    20240221_1_backjoon_10815

--- Feb/20240221_1.cpp
+++ Feb/20240221_1.cpp
@@ -1,4 +1,4 @@
-//백준 11866 요세푸스 문제 0
+//백준 10815 숫자 카드
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -16,27 +16,40 @@
 #define INF 999999999
 using namespace std;
 
-int n,k,m,t,a;
-queue<int> qu;
+int n,k,m,t,a,mid;
+vector<int> v1,v2;
 
 int main() {
     
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cin >> n >> k;
-    for(int i=1;i<=n;i++)
-        qu.push(i);
-    cout << "<";
-    for(int i=1;i<n;i++){
-        for(int j=1;j<k;j++){
-            int a = qu.front();
-            qu.pop();
-            qu.push(a);
-        }
-        cout << qu.front() << ", ";
-        qu.pop();
+    cin >> n;
+    while(n--){
+        cin >> k;
+        v1.push_back(k);
     }
-    cout << qu.front() << ">";
+    sort(v1.begin(),v1.end());
+    cin >> m;
+    while(m--){
+        cin >> k;
+        v2.push_back(k);
+    }
+    
+    for(int i=0;i<v2.size();i++){
+        int lo = 0, hi = v1.size();
+        while (lo + 1 < hi) {
+            mid = (lo + hi) / 2;
+            if(v2[i] == v1[mid]) break;
+            else if (v2[i] > v1[mid]) lo = mid;
+            else hi = mid;
+        }
+        if(lo+1 ==hi)
+            mid = (lo + hi) / 2;
+        if(v1[mid] == v2[i]) cout << 1;
+        else cout << 0;
+        cout << " ";
+    }
+
     
     return 0;
 }
